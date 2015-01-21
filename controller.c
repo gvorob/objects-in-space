0014276f8dd16b8fdf68aaba4d8d1623e58fe080
George Vorobyev <quaffle97@gmail.com>
2015-01-21 10:59:51 -0500

    more stubbing

--- controller.c
+++ controller.c
@@ -1,8 +1,38 @@
 #include "controller.h"
 
 int main(int argc, char *argv[]) {
-	errx(-1, "main not implemented");
+	
+	int is_client = 1; //TEMP
+
+	if(is_client) {
+		init_client("127.0.0.1", 9999);//TEMP
+		//Won't return from this
+	}
+
+	gamestate_struct gs;
+
+	//server
+	init_server(&gs);
+	while(1) {
+		int i;
+
+		for(i = 0; i < MAX_PLAYERS; i++) {
+			if(gs.clients[i].socket_d != -1)
+				get_input(&(gs.clients[i]));
+		}
+		for(i = 0; i < MAX_PLAYERS; i++) {
+			update_input(&(gs.clients[i]),&gs);
+		}
+		update(&gs);
+		for(i = 0; i < MAX_PLAYERS; i++) {
+			render(,gs);
+		}
+	}
 }
+
+
+
+
 
 void get_input(client_struct* c) {
 	errx(-1, "get_input not implemented");
@@ -10,13 +40,16 @@ void get_input(client_struct* c) {
 
 void update_input(client_struct* c, gamestate_struct* gs) {
 	errx(-1, "update_input not implemented");
+	update_input_connecting(c, gs);
 }
 
 void update(gamestate_struct* gs) {
 	//delegates to other files
 	errx(-1, "update not implemented");
+	update_connecting(gs);
 }
 
 void render(client_struct* c, gamestate_struct* gs) {
 	errx(-1, "render not implemented");
+	render_connecting(c, gs);
 }
---
+++ util.h
@@ -0,0 +1,11 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <err.h>
+
+#define PORT 9998
+#define LOCALHOST "127.0.0.1"
+
+
+
+#endif
